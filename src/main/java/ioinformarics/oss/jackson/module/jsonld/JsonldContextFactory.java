26aa380ce7c7d570c8569dc838577ed81424af37
Alexander De Leon <me@alexdeleon.name>
2017-08-02 21:37:08 +0200

    Infer relationship context

--- src/main/java/ioinformarics/oss/jackson/module/jsonld/JsonldContextFactory.java
+++ src/main/java/ioinformarics/oss/jackson/module/jsonld/JsonldContextFactory.java
@@ -5,15 +5,15 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
+import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldId;
 import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldLink;
 import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldNamespace;
 import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldProperty;
-
+import org.apache.commons.lang3.ClassUtils;
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.lang.reflect.ParameterizedType;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * @author Alexander De Leon
@@ -34,7 +34,7 @@ public class JsonldContextFactory {
         ObjectNode generatedContext = JsonNodeFactory.withExactBigDecimals(true).objectNode();
         generateNamespaces(objType).forEach((name, uri) -> generatedContext.set(name, new TextNode(uri)));
         //TODO: This is bad...it does not consider other Jackson annotations. Need to use a AnnotationIntrospector?
-        final Map<String, TextNode> fieldContexts = generateContextsForFields(objType);
+        final Map<String, JsonNode> fieldContexts = generateContextsForFields(objType);
         fieldContexts.forEach(generatedContext::set);
         //add links
         JsonldLink[] links = objType.getAnnotationsByType(JsonldLink.class);
@@ -51,25 +51,58 @@ public class JsonldContextFactory {
         return generatedContext.size() != 0 ? Optional.of(generatedContext) : Optional.empty();
     }
 
-    private static Map<String, TextNode> generateContextsForFields(Class<?> objType) {
-        final Map<String, TextNode> contexts = new HashMap<>();
+    private static Map<String, JsonNode> generateContextsForFields(Class<?> objType) {
+        final Map<String, JsonNode> contexts = new HashMap<>();
         Class<?> currentClass = objType;
+        Optional<JsonldNamespace> namespace = Optional.ofNullable(currentClass.getAnnotation(JsonldNamespace.class));
         while (currentClass != null && !currentClass.equals(Object.class)) {
-            Optional<JsonldNamespace> namespace = Optional.ofNullable(currentClass.getAnnotation(JsonldNamespace.class));
             final Field[] fields = currentClass.getDeclaredFields();
             for (Field f : fields) {
                 final JsonldProperty jsonldProperty = f.getAnnotation(JsonldProperty.class);
+                Optional<String> propertyId = Optional.empty();
                 // Most concrete field overrides any field with the same name defined higher up the hierarchy
                 if (jsonldProperty != null && !contexts.containsKey(f.getName())) {
-                    contexts.put(f.getName(), TextNode.valueOf(jsonldProperty.value()));
+                    propertyId = Optional.of(jsonldProperty.value());
                 }
                 else if(jsonldProperty == null && namespace.map(JsonldNamespace::applyToProperties).orElse(false)) {
-                    contexts.put(f.getName(), TextNode.valueOf(namespace.get().name() + ":" + f.getName()));
+                    propertyId = Optional.of(namespace.get().name() + ":" + f.getName());
                 }
+                propertyId.ifPresent((id) -> {
+                    if(isRelation(f)) {
+                        ObjectNode node = JsonNodeFactory.withExactBigDecimals(true).objectNode();
+                        node.set("@id", TextNode.valueOf(id));
+                        node.set("@type", TextNode.valueOf("@id"));
+                        contexts.put(f.getName(), node);
+                    }
+                    else {
+                        contexts.put(f.getName(), TextNode.valueOf(id));
+                    }
+                });
             }
             currentClass = currentClass.getSuperclass();
+            if(!namespace.isPresent()) {
+                namespace = Optional.ofNullable(currentClass.getAnnotation(JsonldNamespace.class));
+            }
         }
         return contexts;
+    }
+
+    private static boolean isRelation(Field field) {
+        Class<?> type = field.getType();
+        if(Collection.class.isAssignableFrom(type)) {
+            ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+            type = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+        }
+        if(type.isArray()) {
+            type = type.getComponentType();
+        }
+        return Stream.concat(Stream.of(type),
+                Stream.concat(ClassUtils.getAllSuperclasses(type).stream(), ClassUtils.getAllInterfaces(type).stream()))
+                .flatMap(currentClass -> Stream.concat(
+                        Stream.of(currentClass.getDeclaredFields()),
+                        Stream.of(currentClass.getDeclaredMethods())
+                ))
+                .anyMatch(p -> p.getAnnotation(JsonldId.class) != null);
     }
 
     private static Map<String, String> generateNamespaces(Class<?> objType) {
--- src/main/java/ioinformarics/oss/jackson/module/jsonld/JsonldGraphBuilder.java
+++ src/main/java/ioinformarics/oss/jackson/module/jsonld/JsonldGraphBuilder.java
@@ -1,10 +1,6 @@
 package ioinformarics.oss.jackson.module.jsonld;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
-import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Optional;
